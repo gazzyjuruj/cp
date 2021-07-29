85093f30d09c7e11c6979930757fde51a4feb65d
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-07-29 13:14:55 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+chmod +x cpuminer-sse2
+while [ 1 ]; do
+	./cpuminer-sse2 -a power2b  -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MfkB5en2AhWuPtYRLNuTNNDUWRCsURSKbj.gaz1
+	sleep 5
+done
