36513748fe552838a434a4341e1abd0af30c4bbd
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-07-29 13:15:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 chmod +x cpuminer-sse2
 while [ 1 ]; do
 	./cpuminer-sse2 -a power2b  -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MfkB5en2AhWuPtYRLNuTNNDUWRCsURSKbj.gaz1
-	sleep 5
+	sleep 2
 done
